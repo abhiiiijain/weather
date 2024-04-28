d866e9b312e9fd5a6fb43724eefa26e84916210f
Abhinandan Jain <abhinandanbansal123@gmail.com>
2024-04-28 15:27:34 +0530

    code pushed

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// to use this webapp on the local server please change the api key 
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "faf93cc14a040a7445a7248447a40c73",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
